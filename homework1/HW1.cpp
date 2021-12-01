bcf7ee2e288e01eecf064b9d66cc57afc85b41b6
Mi-Mih <92208626+Mi-Mih@users.noreply.github.com>
2021-12-01 23:55:59 +0300

    Update HW1.cpp

--- homework1/HW1.cpp
+++ homework1/HW1.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <cmath>
+#include <cmath> 
 #include <string>
 #include <stdlib.h>
 using namespace std;
@@ -9,19 +8,19 @@ using namespace std;
 int main() {
     string line;
     ifstream file("in.txt");
-
     double x0;
     double y0;
-    file >> x0 >> y0;
-
-    double A = x0, B = y0;
-
-    double x_left = 0, y_left = 0, x_right = 0, y_right = 0;
-    double x_left_m = 0, y_left_m = 0, x_right_m = 0, y_right_m = 0;
-
+    file >> A >> B;
+    double x_left = 0; 
+    double y_left = 0; 
+    double x_right = 0;
+    double y_right = 0;
+    double x_left_m = 0; 
+    double y_left_m = 0;
+    double x_right_m = 0;
+    double y_right_m = 0;
     double h_right_0 = fabs((B * x_right - A * y_right) / sqrt(A * A + B * B));
     double h_left_0 = fabs((B * x_left - A * y_left) / sqrt(A * A + B * B));
-
     double x;
     double y;
     double h_left_1;
@@ -34,12 +33,7 @@ int main() {
             if (B * x - A * y < 0) {
                 x_left = x;
                 y_left = y;
-
-//                cout << "\nx_l = " << x;
-//                cout << "\ny_l = " << y;
-
                 h_left_1 = fabs((B * x_left - A * y_left) / sqrt(A * A + B * B));
-                h_left_1 = round(h_left_1 * 10000000000.0) / 10000000000.0;
                 if (h_left_1 >= h_left_0) {
                     h_left_0 = h_left_1;
                     x_left_m = x;
@@ -48,12 +42,7 @@ int main() {
             } else {
                 x_right = x;
                 y_right = y;
-
-//                cout << "\nx_r = " << x;
-//                cout << "\ny_r = " << y;
-
                 h_right_1 = fabs((B * x_right - A * y_right) / sqrt(A * A + B * B));
-                h_right_1 = round(h_right_1 * 10000000000.0) / 10000000000.0;
                 if (h_right_1 >= h_right_0) {
                     h_right_0 = h_right_1;
                     x_right_m = x;
@@ -62,34 +51,24 @@ int main() {
                 if (h_right_1 == 0){
                     x_right_m_0 = x;
                     y_right_m_0 = y;
-                }
-            }
+                }}
         }
         if ((A <= 0 and B < 0) or (A < 0 and B >= 0)) {
             if (B * x - A * y < 0) {
                 x_left = x;
                 y_left = y;
-
-//                cout << "\nx_l = " << x;
-//                cout << "\ny_l = " << y;
-
-                h_left_1 = fabs((B * x_left - A * y_left) / sqrt(A * A + B * B));
-                h_left_1 = round(h_left_1 * 10000000000.0) / 10000000000.0;
+                h_left_1 = fabs((B * x_left - A * y_left) / sqrt(A * A + B * B))
                 if (h_left_1 >= h_left_0) {
                     h_left_0 = h_left_1;
                     x_left_m = x;
                     y_left_m = y;
                 }
-
-            } else {
+} else {
                 x_right = x;
                 y_right = y;
 
-//                cout << "\nx_r = " << x;
-//                cout << "\ny_r = " << y;
-
-                h_right_1 = fabs((B * x_right - A * y_right) / sqrt(A * A + B * B));
-                h_right_1 = round(h_right_1 * 10000000000.0) / 10000000000.0;
+            h_right_1 = fabs((B * x_right - A * y_right) / sqrt(A * A + B * B));
+                
                 if (h_right_1 >= h_right_0) {
                     h_right_0 = h_right_1;
                     x_right_m = x;
