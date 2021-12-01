cc945a52e812e5892959819c087fcd8770194312
Mi-Mih <92208626+Mi-Mih@users.noreply.github.com>
2021-12-02 00:31:29 +0300

    Update HW1.cpp

--- homework1/HW1.cpp
+++ homework1/HW1.cpp
@@ -1,64 +1,63 @@
 #include <iostream>
+#include <iostream>
 #include <fstream>
 #include <cmath> 
 #include <string>
 #include <stdlib.h>
 using namespace std;
-
 int main() {
     string line;
-    ifstream file("in.txt");
+    ifstream Input_data("in.txt");
     double X_0;
     double Y_0;
-    file >> X_0 >> Y_0;
-    double x_left = 0; 
-    double y_left = 0; 
-    double x_right = 0;
-    double y_right = 0;
-    double x_left_m = 0; 
-    double y_left_m = 0;
-    double x_right_m = 0;
-    double y_right_m = 0;
-    double h_right_0 = fabs((Y_0 * x_right - X_0 * y_right) / sqrt(X_0 * X_0 + Y_0 * Y_0));
-    double h_left_0 = fabs((Y_0 * x_left - X_0 * y_left) / sqrt(X_0 * X_0 + Y_0 * Y_0));
+    Input_data >> X_0 >> Y_0;
+    double X_L = 0; 
+    double Y_L = 0; 
+    double X_R = 0;
+    double Y_R = 0;
+    double X_Lm = 0; 
+    double Y_Lm = 0;
+    double X_Rmax = 0;
+    double Y_Lmax = 0;
+    double H_Rstart = fabs((Y_0 * X_R - X_0 * Y_R) / sqrt(X_0 * X_0 + Y_0 * Y_0));
+    double H_Lstart = fabs((Y_0 * X_L - X_0 * Y_L) / sqrt(X_0 * X_0 + Y_0 * Y_0));
     double x;
     double y;
-    double h_left_1;
-    double h_right_1;
-    double y_right_m_0;
-    double x_right_m_0;
-    while (file >> x >> y) {
+    double H_L1;
+    double H_R1;
+    double Y_Rms;
+    double X_Rms;
+    while (Input_data >> x >> y) {
                    if (Y_0 * x - X_0 * y < 0) {
-                x_left = x;
-                y_left = y;
-                h_left_1 = fabs((Y_0 * x_left - X_0 * y_left) / sqrt(X_0 * X_0 + Y_0 * Y_0));
-                if (h_left_1 >= h_left_0) {
-                    h_left_0 = h_left_1;
-                    x_left_m = x;
-                    y_left_m = y;
+                X_L = x;
+                Y_L = y;
+                H_L1 = fabs((Y_0 * X_L - X_0 * Y_L) / sqrt(X_0 * X_0 + Y_0 * Y_0));
+                if (H_L1 >= H_Lstart) {
+                    H_Lstart = H_L1;
+                    X_Lm = x;
+                    Y_Lm = y;
                 }
             } else {
-                x_right = x;
-                y_right = y;
-                h_right_1 = fabs((Y_0 * x_right - X_0 * y_right) / sqrt(X_0 * X_0 + Y_0 * Y_0));
-                if (h_right_1 >= h_right_0) {
-                    h_right_0 = h_right_1;
-                    x_right_m = x;
-                    y_right_m = y;
+                X_R = x;
+                Y_R = y;
+                H_R1 = fabs((Y_0 * X_R - X_0 * Y_R) / sqrt(X_0 * X_0 + Y_0 * Y_0));
+                if (H_R1 >= H_Rstart) {
+                    H_Rstart = H_R1;
+                    X_Rmax = x;
+                    Y_Lmax = y;
                 }
-                if (h_right_1 == 0){
-                    x_right_m_0 = x;
-                    y_right_m_0 = y;
+                if (H_R1 == 0){
+                    X_Rms = x;
+                    Y_Rms = y;
                 }}
         }
-
-    file.close();
-    if (h_right_1 == 0){
-        cout << "Leftmost: " << x_left_m << " " << y_left_m<<"\n";
-        cout << "Rightmost: " << x_right_m_0 << " " << y_right_m_0<<"\n";
+Input_data.close();
+    if (H_R1 == 0){
+        cout << "Leftmost: " << X_Lm << " " << Y_Lm<<"\n";
+        cout << "Rightmost: " << X_Rms << " " << Y_Rms<<"\n";
     }
     else {
-        cout << "Leftmost: " << x_left_m << " " << y_left_m<<"\n";
-        cout << "Rightmost: " << x_right_m << " " << y_right_m<<"\n";
+        cout << "Leftmost: " << X_Lm << " " << Y_Lm<<"\n";
+        cout << "Rightmost: " << X_Rmax << " " << Y_Lmax<<"\n";
     }
 }
