ad90d0bba481aef4055d9871716292a416a9b055
Mi-Mih <92208626+Mi-Mih@users.noreply.github.com>
2021-12-01 23:47:31 +0300

    Update HW2.cpp

--- homework2/HW2.cpp
+++ homework2/HW2.cpp
@@ -4,43 +4,9 @@
 #include <fstream>
 #include <algorithm>
 using namespace std;
-void update_initial_conds(vector<vector<double>> &wall, vector<double> &V) { 
-    reverse(wall.begin(), wall.end());
-    V[1] = V[1] - 9.81 * wall[0][0] / V[0];
-    double x_0 = wall[0][0];
-    wall[0][0] = 0;
-    for (size_t i = 1; i < wall.size(); i++) {
-        wall[i][0] = x_0 - wall[i][0];
-    }
-}
-void update_barriers(vector<vector<double>> &wall, vector<double> &V) {
-    vector<vector<double>> temp_barriers = {wall[0]};
-    double wall_h;
-    for (size_t i = 1; i < wall.size(); i++) {
-        wall_h = wall[0][1] + V[1] / V[0] * wall[i][0] - 9.81 * 0.5 / (V[0]*V[0]) * wall[i][0] * wall[i][0];
-        temp_barriers.push_back({bloatarriers[i][0], wall[i][1], wall[i][2]});
-        if (wall[i][1] >= wall_h) {
-            temp_barriers[i][1] = wall_h;
-            break;    }
-    }
-    wall.swap(temp_barriers);
-}
-double solution(vector<vector<double>> wall, vector<double> V) {
-    while (true) {
-        update_initial_conds(wall, V);
-        if (wall.size() == 2) {
-            if (wall[0][2] > wall[1][2]) {
-                return wall[1][2];
-            } else {
-                return wall[0][2];
-            }
-        }
-        if (wall[0][2] == 0) {
-            return 0;
-        }
-        update_barriers(wall, V);
-    }
-}
+void calculate(vector<vector<double>> &wall, vector<double> &V);
+void wall_func(vector<vector<double>> &wall, vector<double> &V);
+double solution(vector<vector<double>> wall, vector<double> V);
 int main(int argc, char** argv) {
     if(argc < 2 || argc > 2){
         cout << "аргументов нет или их больше чем мы ожидаем"<< endl;
@@ -77,4 +43,41 @@ int main(int argc, char** argv) {
     cout << int(solution(wall, V)) << endl;
     return 0;
 }
-
+void calculate(vector<vector<double>> &wall, vector<double> &V) { 
+    reverse(wall.begin(), wall.end());
+    V[1] = V[1] - 9.81 * wall[0][0] / V[0];
+    double x_0 = wall[0][0];
+    wall[0][0] = 0;
+    for (size_t i = 1; i < wall.size(); i++) {
+        wall[i][0] = x_0 - wall[i][0];
+    }
+}
+void wall_func(vector<vector<double>> &wall, vector<double> &V) {
+    vector<vector<double>> temp_barriers = {wall[0]};
+    double wall_h;
+    for (size_t i = 1; i < wall.size(); i++) {
+        wall_h = wall[0][1] + V[1] / V[0] * wall[i][0] - 9.81 * 0.5 / (V[0]*V[0]) * wall[i][0] * wall[i][0];
+        temp_barriers.push_back({bloatarriers[i][0], wall[i][1], wall[i][2]});
+        if (wall[i][1] >= wall_h) {
+            temp_barriers[i][1] = wall_h;
+            break;
+        }
+    }
+    wall.swap(temp_barriers);
+}
+double solution(vector<vector<double>> wall, vector<double> V) {
+    while (true) {
+        calculate(wall, V);
+        if (wall.size() == 2) {
+            if (wall[0][2] > wall[1][2]) {
+                return wall[1][2];
+            } else {
+                return wall[0][2];
+            }
+        }
+        if (wall[0][2] == 0) {
+            return 0;
+        }
+        wall_func(wall, V);
+    }
+}
